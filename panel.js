4bc2c6ad2deb7a0b3bdfdb4d16d07d803bba385b
Bjorn <krols.bj@gmail.com>
2019-05-14 15:00:13 +0200

    Pad single HMS with 0

--- panel.js
+++ panel.js
@@ -27,8 +27,11 @@ function render() {
         events.forEach(function(event) {
           const { time, propertyId, development, type, ...rest } = event;
           const date = new Date(time);
+          const h = ("0" + date.getHours()).slice(-2);
+          const m = ("0" + date.getMinutes()).slice(-2);
+          const s = ("0" + date.getSeconds()).slice(-2);
           const eventElement = document.createElement("div");
-          eventElement.innerHTML += `<div>${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} - ${propertyId} - ${type}</div>`;
+          eventElement.innerHTML += `<div>${h}:${m}:${s} - ${propertyId} - ${type}</div>`;
           eventElement.innerHTML += `<div><pre>${JSON.stringify(
             rest
           )}</pre></div>`;
