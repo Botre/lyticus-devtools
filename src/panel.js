6c0af1a564db0e9232bdd3143ca72ac006c833c6
Bjorn <krols.bj@gmail.com>
2019-05-14 15:32:56 +0200

    Added date-fns

--- src/panel.js
+++ src/panel.js
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+
 chrome.storage.onChanged.addListener(function() {
   render();
 });
@@ -26,12 +28,11 @@ function render() {
         const eventsContainer = document.createElement("div");
         events.forEach(function(event) {
           const { time, propertyId, development, type, ...rest } = event;
-          const date = new Date(time);
-          const h = ("0" + date.getHours()).slice(-2);
-          const m = ("0" + date.getMinutes()).slice(-2);
-          const s = ("0" + date.getSeconds()).slice(-2);
           const eventElement = document.createElement("div");
-          eventElement.innerHTML += `<div>${h}:${m}:${s} - ${propertyId} - ${type}</div>`;
+          eventElement.innerHTML += `<div>${format(
+            new Date(time),
+            "HH:mm:ss"
+          )} - ${propertyId} - ${type}</div>`;
           eventElement.innerHTML += `<div><pre>${JSON.stringify(
             rest
           )}</pre></div>`;
